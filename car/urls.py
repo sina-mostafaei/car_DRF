b3c655a7097b13a1014d7d03fe87014b79632c7f
Sina Mostafaei <sina84mostafaei@gmail.com>
2026-02-19 11:07:45 +0330

    Update urls.py

--- car/urls.py
+++ car/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('pr', person_view),
-    path('cr', car_view),
+    path('person_view', person_view),
+    path('car_view', car_view),
     path('info', car_info),
-    path('add_frim', firm_adder),
+    path('add_firm', firm_adder),
 
 ]
+

d50e8c33262e44a08a2e929344a9e6ccd978dcad
Sina Mostafaei <sina84mostafaei@gmail.com>
2026-02-19 10:59:57 +0330

    Update serializers.py

--- car/serializers.py
+++ car/serializers.py
@@ -1,27 +1,36 @@
 from . import models as m
 from rest_framework import serializers
 
+
 class per_ser(serializers.ModelSerializer):
     class Meta:
-        model = m.person
-        fields="__all__"
+        model = m.Person
+        fields = "__all__"
+
+
 class car_ser(serializers.ModelSerializer):
     class Meta:
-        model=m.car
-        fields="__all__"
+        model = m.Car
+        fields = "__all__"
+
+
 class infouw(serializers.ModelSerializer):
     # person=m.person
     class Meta:
-        model=m.car
-        fields="__all__"
-        depth=1
+        model = m.Car
+        fields = "__all__"
+        depth = 1
+
+
 class frim_ser(serializers.ModelSerializer):
     class Meta:
-        model= m.frim
-        fields="__all__"
+        model = m.Firm
+        fields = "__all__"
+
+
 class inpho(serializers.ModelSerializer):
     # person=m.person
     class Meta:
-        model=m.frim
-        fields="__all__"
-        depth=1
+        model = m.Firm
+        fields = "__all__"
+        depth = 1

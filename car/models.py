8a6ebed998c0dcbd33977b3d73b5024bcf31bb56
Sina Mostafaei <sina84mostafaei@gmail.com>
2026-02-19 10:58:48 +0330

    Update models.py

--- car/models.py
+++ car/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 
-# Create your models here.
 
 
-class person(models.Model):
+
+class Person(models.Model):
     name = models.CharField(max_length=30)
-    moobile = models.IntegerField()
-    date = models.DateTimeField(auto_now=True, auto_now_add=False)
+    mobile = models.CharField(max_length=30)
+    date = models.DateTimeField(auto_now=True)
 
 
-class frim(models.Model):
-    title=models.CharField(max_length=20)
-    membership_code=models.IntegerField()
+class Firm(models.Model):
+    title = models.CharField(max_length=20)
+    membership_code = models.IntegerField()
 
-class car(models.Model):
+
+class Car(models.Model):
     company = models.CharField(max_length=20)
     price = models.IntegerField()
-    owner = models.ForeignKey(person, on_delete=models.CASCADE)
-    store = models.ForeignKey(frim, null=True,blank=True,on_delete=models.CASCADE)
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+    store = models.ForeignKey(
+        Firm, null=True, blank=True, on_delete=models.CASCADE)

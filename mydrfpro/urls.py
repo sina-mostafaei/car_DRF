e41670f28f4c22cffc6a26b4c0bc4dc4f440f9c2
Sina Mostafaei <sina84mostafaei@gmail.com>
2026-02-22 02:05:32 +0330

    Update urls.py

--- mydrfpro/urls.py
+++ mydrfpro/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path ,  include
-
+from django.urls import path,  include
+from rest_framework.authtoken import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('car/',include("car.urls")),
+    path('car/', include("car.urls")),
+    path('api_token_auth/', views.obtain_auth_token),
+    path('api_auth/', include("rest_framework.urls")),
+    path('user/', include("user.urls")),
+
 ]

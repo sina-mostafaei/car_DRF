b93e02f6610e04b02c9a6f08a890516edb5c8975
Sina Mostafaei <sina84mostafaei@gmail.com>
2026-02-22 02:04:23 +0330

    Update settings.py

--- mydrfpro/settings.py
+++ mydrfpro/settings.py
@@ -39,7 +39,27 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'car',
+    'rest_framework.authtoken',
+    'django_filters',
+    'user',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+        # 'rest_framework.permissions.IsAdminUser'
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+    "DEFAULT_PAGINATION_CLASS":"rest_framework.pagination.LimitOffsetPagination","PAGE_SIZE":2
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

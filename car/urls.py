66d6df9f06b003332cca279df82afee5ed233641
Sina Mostafaei <sina84mostafaei@gmail.com>
2026-02-22 09:29:21 +0330

    Update urls.py

--- car/urls.py
+++ car/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register("person_view", person_view)
+router.register("car_view", car_view)
+router.register("firm_view", firm_view)
+router.register("info", infow,basename="cars_info")
 urlpatterns = [
-    path('person_view', person_view),
-    path('car_view', car_view),
-    path('info', car_info),
-    path('add_firm', firm_adder),
-
 ]
 
+urlpatterns += router.urls
